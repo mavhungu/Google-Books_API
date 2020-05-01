0b6a2485c2c22afaccfa05e863b3b395eb38332a
ronewa <ronewamavhungu@gmail.com>
2020-05-01 20:25:14 +0200

    image mb3

--- src/components/profile/me.js
+++ src/components/profile/me.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Small from '../img/small2.png';
+import Small from '../img/small.png';
 import "tachyons";
 
  const Me = ({onRouteChange})=>{
      return(
         <div className={'mw9 cf fl w-90 bg-light-green pa3 ma2 dib br grow'}>
-            <div>{'Books will show here'}</div>
+            <div className={'mb3'}>{'Books will show here'}</div>
             <img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2" alt='profile' />
         </div>
      )
