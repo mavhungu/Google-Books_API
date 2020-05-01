9e2875a8d65e4c6aacbe69cfac5a9efab1b6970b
ronewa <ronewamavhungu@gmail.com>
2020-05-01 19:38:35 +0200

    changing App name

--- src/components/profile/me.js
+++ src/components/profile/me.js
@@ -4,10 +4,10 @@ import "tachyons";
 
  const Me = ({onRouteChange})=>{
      return(
-         <div className={'mw9 cf fl w-90 bg-light-green pa3 ma2 dib br grow'}>
-             <div>{'Books will show here'}</div>
-             <img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2" alt='profile' />
-         </div>
+        <div className={'mw9 cf fl w-90 bg-light-green pa3 ma2 dib br grow'}>
+            <div>{'Books will show here'}</div>
+            <img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2" alt='profile' />
+        </div>
      )
  }
  export default Me;
