69942c7f8df3849206378ed616bee22c01dd0611
ronewa <ronewamavhungu@gmail.com>
2020-05-01 20:49:15 +0200

    adding mt4 and ml3

--- src/components/profile/me.js
+++ src/components/profile/me.js
@@ -4,7 +4,7 @@ import "tachyons";
 
  const Me = ({onRouteChange})=>{
      return(
-        <div className={'mw9 cf fl w-90 bg-light-green pa3 ma2 dib br grow'}>
+        <div className={'mw9 cf fl w-90 bg-light-green pa3 mt4 ml3 dib br grow'}>
             <div className={'mb3'}>{'Books will show here'}</div>
             <img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2" alt='profile' />
         </div>
