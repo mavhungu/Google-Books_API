9cbb6f17b45225a0daacbb7617956f91ed1bd686
ronewa <ronewamavhungu@gmail.com>
2020-05-03 15:09:36 +0200

    adding more info on placeholder

--- src/components/seach/search.js
+++ src/components/seach/search.js
@@ -10,7 +10,7 @@ const Search = ({OnSearchInput,OnSubmitButton,onRouteChange})=>{
                     <div className="cf">
                         <label className="clip" htmlFor="email-address">Email Address</label>
                         <input require="true" className="f6 f5-l input-reset bn fl black-80 b--green bg-lightest-blue pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
-                            placeholder="Search" type="text" name="search-book"
+                            placeholder="Search Books" type="text" name="search-book"
                             id="search-book" onChange={OnSearchInput} />
                             <input
                                 className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
