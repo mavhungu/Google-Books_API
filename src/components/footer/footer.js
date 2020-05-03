25861056f0b4ba68cbbdc72875ed102bd0e449f6
ronewa <ronewamavhungu@gmail.com>
2020-05-03 17:57:03 +0200

    adding links to footer icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,21 +4,21 @@ import 'tachyons';
 const Footer = () => {
     return (
         <footer className="pv3 ph3 ph5-ns bg-black-90">
-            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="/" title="">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" target={'_blank'} rel='noopener noreferrer' href="https://www.facebook.com/ronewa.gilbert" title="">
                 <svg data-icon="facebook" viewBox="0 0 32 32" style={{fill:'currentcolor'}}>
                     <title>facebook icon</title>
                     <path
                         d={"M8 12 L13 12 L13 8 C13 2 17 1 24 2 L24 7 C20 7 19 7 19 10 L19 12 L24 12 L23 18 L19 18 L19 30 L13 30 L13 18 L8 18 z"}></path>
                 </svg>
             </a>
-            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="/" title="">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" target={'_blank'} rel='noopener noreferrer' href="https://twitter.com/MavhunguRgb" title="">
                 <svg data-icon="twitter" viewBox="0 0 32 32" style={{fill:'currentcolor'}}>
                     <title>twitter icon</title>
                     <path
                         d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4"></path>
                 </svg>
             </a>
-            <a className="link dim gray dib br-100 h2 w2 mr3 pa2 bg-near-white ba b--black-10" href="/" title="">
+            <a className="link dim gray dib br-100 h2 w2 mr3 pa2 bg-near-white ba b--black-10" target={'_blank'} rel='noopener noreferrer' href="https://github.com/mavhungu" title="">
                 <svg data-icon="github" viewBox="0 0 32 32" style={{fill:'currentcolor'}}>
                     <title>github icon</title>
                     <path
