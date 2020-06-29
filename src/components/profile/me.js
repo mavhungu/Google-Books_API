f63d70a25b185decca92e7d22729a07f22e36fe1
ronewa <ronewamavhungu@gmail.com>
2020-06-29 19:17:05 +0200

    reversing to original profile file

--- src/components/profile/me.js
+++ src/components/profile/me.js
@@ -4,12 +4,10 @@ import "tachyons";
 
  const Me = ({onRouteChange})=>{
      return(
-		<div className={''}>
-			<div className={'mw9 cf fl w-100 bg-light-green pa3 mt4 dib br'}>
-				{/*<div className={'mb3 w-100 bg-white pa3 b pointer books'}>{'Books will show here'}</div>*/}
-				<img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2 grow pointer" alt='profile' />
-			</div>
-		</div>
+        <div className={'mw9 cf fl w-100 bg-light-green pa3 mt4 dib br'}>
+            <div className={'mb3 w-100 bg-white pa3 b pointer books'}>{'Books will show here'}</div>
+            <img src={Small} className="br-100 h4 w4 dib ba b--black-05 pa2 grow pointer" alt='profile' />
+        </div>
      );
  }
  export default Me;
